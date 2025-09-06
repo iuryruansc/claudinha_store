47eb5177d5e3a6d96c377c25748d4e52f7ba23be
iuryruansc <iuryruansc@gmail.com>
2025-09-06 20:13:12 -0300

    removing console.logs

--- controllers/movEstoqueController.js
+++ controllers/movEstoqueController.js
@@ -7,9 +7,6 @@ const { getAllMovs } = require('../services/movEstoqueService');
 router.get('/movimentacoes', asyncHandler(async (req, res) => {
     const {movs, funcionarios, produtos} = await getAllMovs();
 
-    console.log(movs[1].estoque);
-    
-
     res.render('admin/mov-estoque/index', { movs, funcionarios, produtos, formatDate })
 }));
 
